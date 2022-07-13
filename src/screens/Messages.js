8d72c0b3e1104198697765d3da46d6d01c3406c8
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-07-13 12:20:54 +0300

    feat: created other screens

---
+++ src/screens/Messages.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Messages = () => {
+  return (
+    <View>
+      <Text>Messages</Text>
+    </View>
+  );
+};
+
+export default Messages;
